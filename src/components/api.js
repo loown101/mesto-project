a6bbeeaeb120563a56bb47b46f3b6f07a10254a5
Lyubov Vasileva <93537611+Luba-web@users.noreply.github.com>
2022-04-14 19:06:21 +0300

    feat: Api.js class

--- src/components/api.js
+++ src/components/api.js
@@ -1,87 +1,76 @@
-const configCohort = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-8/',
-  headers: {
-    'Authorization': 'ea0f5ff1-1f3b-4f72-b28c-164c5b7a6982',
-    'Content-type': 'application/json',
+export default class Api {
+  constructor(data) {
+    this._headers = data.headers;
+    this._baseUrl = data.baseUrl;
+    this._checkResponse = function (res) {
+      console.log(res.json());
+      return res.ok ? res.json() : Promise.reject(res);
+    };
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}users/me`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getCards() {
+    return fetch(`${this._baseUrl}cards`, {
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  editUserInfo(nameInputValue, jobInputValue) {
+    return fetch(`${this._baseUrl}users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: `${nameInputValue}`,
+        about: `${jobInputValue}`,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  addCard(placeLinkValue, placeDescriptionValue) {
+    return fetch(`${this._baseUrl}cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: `${placeDescriptionValue}`,
+        link: `${placeLinkValue}`,
+      }),
+    }).then(this._checkResponse);
+  }
+
+  deleteCard(cardID) {
+    return fetch(`${this._baseUrl}cards/${cardID}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  getlikeCard(cardID, method) {
+    return fetch(`${this._baseUrl}cards/likes/${cardID}`, {
+      method: method,
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
+
+  editAvatar(avatarInputValue) {
+    return fetch(`${this._baseUrl}users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: `${avatarInputValue}`,
+      }),
+    }).then(this._checkResponse);
   }
 }
 
-const checkResponse = (res) => {
-  return res.ok ? res.json() : Promise.reject(res)
-}
-
-function getUserInfo() {
-  return fetch(`${configCohort.baseUrl}users/me`, {
-    headers: configCohort.headers,
-  })
-    .then(checkResponse)
-}
-
-function getCards() {
-  return fetch(`${configCohort.baseUrl}cards`, {
-    headers: configCohort.headers,
-  })
-    .then(checkResponse)
-}
-
-function editUserInfo(nameInputValue, jobInputValue) {
-  return fetch(`${configCohort.baseUrl}users/me`, {
-    method: 'PATCH',
-    headers: configCohort.headers,
-    body: JSON.stringify({
-      name: `${nameInputValue}`,
-      about: `${jobInputValue}`,
-    })
-  })
-    .then(checkResponse)
-}
-
-function addCard(placeLinkValue, placeDescriptionValue) {
-  return fetch(`${configCohort.baseUrl}cards`, {
-    method: 'POST',
-    headers: configCohort.headers,
-    body: JSON.stringify({
-      name: `${placeDescriptionValue}`,
-      link: `${placeLinkValue}`,
-    })
-  })
-    .then(checkResponse)
-}
-
-function deleteCard(cardID) {
-  return fetch(`${configCohort.baseUrl}cards/${cardID}`, {
-    method: 'DELETE',
-    headers: configCohort.headers,
-  })
-    .then(checkResponse)
-}
-
-function getlikeCard(cardID, method) {
-  return fetch(`${configCohort.baseUrl}cards/likes/${cardID}`, {
-    method: method,
-    headers: configCohort.headers,
-  })
-    .then(checkResponse)
-}
-
-function editAvatar(avatarInputValue) {
-  return fetch(`${configCohort.baseUrl}users/me/avatar`, {
-    method: 'PATCH',
-    headers: configCohort.headers,
-    body: JSON.stringify({
-      avatar: `${avatarInputValue}`,
-    })
-  })
-    .then(checkResponse)
-}
-
-export {
-  getUserInfo,
-  getCards,
-  addCard,
-  editUserInfo,
-  deleteCard,
-  getlikeCard,
-  editAvatar,
-};
-
+const api = new Api({
+  baseUrl: "https://nomoreparties.co/v1/plus-cohort-8/",
+  headers: {
+    Authorization: "ea0f5ff1-1f3b-4f72-b28c-164c5b7a6982",
+    "Content-type": "application/json",
+  },
+});
